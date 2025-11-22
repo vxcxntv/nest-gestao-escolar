d2949b19c5284b88ec6e4ebd001b0781a6775388
vxcxntv <contato.mtvicente@gmail.com>
2025-11-21 22:40:04 -0300

    json swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -56,6 +57,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
 
   // A documentação ficará disponível em http://localhost:3000/api-docs
   SwaggerModule.setup('api-docs', app, document);
